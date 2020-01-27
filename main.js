c974917855d6f1005f2a99bd023a95935aaa19a2
sawzedong <48119998+sawzedong@users.noreply.github.com>
2020-01-27 12:22:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 /**INFORMATION**/
 var info = [
-  //["pic1", 123, "crazy", "picone.png"],
-  //["pic2", 234, "mad", "pictwin.png"],
-  //["pic3", 345, "stupid", "picthree.png"]
-  ["Cheegy", 9, "Unknown Species", "picone.png"],
-  ["Turtle", 6, "Turtle", "picone.png"],
-  ["Chirper", 9.5, "Bird", "picone.png"],
-  ["Little Fluff", 7, "Bear", "picone.png"],
-  ["Waddles", 15.5, "Penguin", "picone.png"],
-  ["Kickly", 19, "Rabbit", "picone.png"],
-  ["Elephantny", 5, "Elephant", "picone.png"],
-  ["Liony", 5, "Lion", "picone.png"],
-  ["Black Beak", 25, "Penguin", "picone.png"],
-  ["Bear Bear", 13, "Bear", "picone.png"],
-  ["Hippo", 29, "Hippo", "picone.png"]
+  ["Chirper", 8, "Bird", "Chirper.jpg"],
+  ["Cheeper", 4.5, "Bird", "Cheeper.jpg"],
+  ["Chirpy", 4.5, "Bird", "Chirpy.jpg"],
+  ["Cheepy", 4.5, "Bird", "Cheepy.jpg"],
+  ["Black Beak", 24.5, "Penguin", "Black-Beak.jpg"],
+  ["Waddles", 15, "Penguin", "Waddles.jpg"],
+  ["Elephatny", 9, "Elephant", "Elephantny.jpg"],
+  ["Bear Bear", 13.5, "Bear", "Bear-Bear.jpg"],
+  ["Turtle", 4, "Turtle", "Turtle.jpg"],
+  ["Hippo", 30, "Hippo", "Hippo.jpg"],
+  ["Kickly", 16.5, "Rabbit", "Kickly.jpg"],
+  ["Liony", 5.5, "Lion", "Liony.jpg"],
+  ["Scavenge Boy", 23, "Cat", "Scavenge-Boy.jpg"],
+  ["Lamby", 8.5, "Sheep", "Lamby.jpg"],
+  ["Sheepy", 21, "Sheep", "Sheephy.jpg"],
+  ["Kitty", 16.5, "Cat", "Kitty.jpg"]
 ];
 function myFunction() {
   var myValName = document.getElementById("fname").value;
