5fcd6f5d908ed9265de40ac24f3e2203e66f2cea
sawzedong <48119998+sawzedong@users.noreply.github.com>
2020-01-27 17:37:38 +0800

    Update tag_search.js

--- scripts/tag_search.js
+++ scripts/tag_search.js
@@ -39,7 +39,7 @@ var info = [
               "</div>";
           }
         }
-        if(myDiv.innerHTML = ""){
+        if(myDiv.innerHTML == ""){
         myDiv.innerHTML = "<h1>No such species found</h1>"
         }
       }
