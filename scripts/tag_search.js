90b0be9f7962aa6aac46ca95de5b30dcb40102dd
sawzedong <48119998+sawzedong@users.noreply.github.com>
2020-01-28 16:05:42 +0800

    Update tag_search.js

--- scripts/tag_search.js
+++ scripts/tag_search.js
@@ -1,11 +1,5 @@
-var inputBarItem = document.getElementById("findtag");
-      inputBarItem.addEventListener("keyup", function(event) {
-        if (event.keyCode === 13) {
-          document.getElementById("myBtn").click();
-        }
-      });
 
-var info = [
+      var info = [
         ["Chirper", 8, "Bird", "images/Chirper.jpg"],
         ["Cheeper", 4.5, "Bird", "images/Cheeper.jpg"],
         ["Chirpy", 4.5, "Bird", "images/Chirpy.jpg"],
@@ -24,29 +18,61 @@ var info = [
         ["Kitty", 16.5, "Cat", "images/Kitty.jpg"],
         ["Cheegy", 7, "Unknown", "images/Cheegy.jpg"]
       ];
+      var species = [
+        "Bird",
+        "Penguin",
+        "Elephant",
+        "Bear",
+        "Turtle",
+        "Hippo",
+        "Rabbit",
+        "Lion",
+        "Cat",
+        "Sheep",
+        "Unknown"
+      ];
+      //console.log(species.length);
+      function addCheckBox() {
+        for (var c = 0; c < species.length; c++) {
+          document.getElementById("checkboxes").innerHTML +=
+            "<input type = 'checkbox' class = 'checkBoxes' value = '" +
+            species[c] +
+            "'>" +
+            species[c] +
+            "<br>";
+        }
+      }
       /**INFORMATION END*/
       function myFunction() {
         var outputArray = [];
+        var selected = [];
         var myDiv = document.getElementById("outputarea");
         myDiv.innerHTML = "";
-        var myValTag = document.getElementById("findtag").value;
-        for (var i = 0; i < info.length; i++) {
-          if (info[i][2] === myValTag) {
-            myDiv.innerHTML +=
-              "<div>" +
-              "<h3>Name: " +
-              info[i][0] +
-              "</h3><br><h3>Height: " +
-              info[i][1] +
-              "cm</h3><br><h3>Species: " +
-              info[i][2] +
-              "</h3><br><img src='" +
-              info[i][3] +
-              "' height = 100px width = 100px>" +
-              "</div>";
+        var myValTag = document.getElementsByClassName("checkBoxes");
+        for (var cb = 0; cb < myValTag.length; cb++) {
+          if (myValTag[cb].checked) {
+            selected.push(myValTag[cb].value);
           }
         }
-        if(myDiv.innerHTML == ""){
-        myDiv.innerHTML = "<h1>No such species found</h1>"
+        for (var i = 0; i < info.length; i++) {
+          for (var ij = 0; ij < selected.length; ij++) {
+            if (info[i][2] === selected[ij]) {
+              myDiv.innerHTML +=
+                "<div>" +
+                "<h3>Name: " +
+                info[i][0] +
+                "</h3><br><h3>Height: " +
+                info[i][1] +
+                "cm</h3><br><h3>Species: " +
+                info[i][2] +
+                "</h3><br><img src='" +
+                info[i][3] +
+                "' height = 100px width = 100px>" +
+                "</div>";
+            }
+          }
+        }
+        if (myDiv.innerHTML == "") {
+          myDiv.innerHTML = "<h1>Please select at least one box.</h1>";
         }
       }
